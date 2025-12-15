a991f78d61d18fa6bca1af966bc6c4f183983eea
KingOfTheWeb <awwalsalakomuhammed@gmail.com>
2025-12-15 17:22:36 +0100

    post and category migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
-{
+{ 
     use WithoutModelEvents;
 
     /**
@@ -15,11 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+      
     }
 }
